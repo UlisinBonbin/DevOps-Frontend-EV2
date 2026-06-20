27ad6ca73919c62f551de8004e59e7626e40eca0
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-06-20 00:08:47 -0400

    Cambios de prueba

--- src/componentes/Layouts/Footer.jsx
+++ src/componentes/Layouts/Footer.jsx
@@ -74,7 +74,7 @@ function Footer() {
             <a href="#" className="hover:underline">
               ITPCARGO™
             </a>
-            . Todos los derechos reservados-cambio de prueba, cambios de prueba OwO UWU, otra mas.
+            . Todos los derechos reservados-cambio de prueba, cambios de prueba OwO UWU, otra mas, hola.
           </span>
         </div>
       </div>
